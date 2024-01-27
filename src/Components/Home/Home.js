c3f2f85d18a134b84441a7188b68208ec53a90cd
zohaadafzal23 <zohaad.afzal@sadapay.pk>
2024-01-27 17:30:20 +0500

    Converted into react fixes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -20,10 +20,362 @@ import layer_1_6 from "../assets/images/Layer_1 (6).png";
 import layer_1_7 from "../assets/images/Layer_1 (7).png";
 import layer_1_8 from "../assets/images/Layer_1 (8).png";
 import isolationMode from "../assets/images/Isolation_Mode (1).png";
+import MaskGroup from "../assets/images/Mask-group.png"
+import { gsap, ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { useEffect } from "react";
+import $ from 'jquery';
+import SplitType from 'split-type'
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+gsap.registerPlugin(ScrollTrigger)
+
+
 
 const Home = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".page__wrap",
+      { y: "0.3em" },
+      {
+        opacity: 1,
+        y: "0em",
+        duration: 0.3,
+        ease: "power1.inOut",
+      }
+    );
+
+
+
+    let tl = gsap.timeline({
+      delay: 0.5,
+      defaults: { duration: 0.2, ease: "power4.out" },
+    });
+
+    function progressUpdate() {
+      let progress = Math.round(this.progress() * 99);
+      $(".loader_number").text(progress);
+    }
+
+    tl.to(".loader_bottom_bar", {
+      width: "100%",
+      duration: 2.7,
+      onUpdate: progressUpdate,
+      ease: "power4.out", // Add your desired easing function here
+    }).set(".loader", {
+      display: "none",
+    });
+
+
+    function animateText(textClass, targetClass) {
+      let textElements = document.querySelectorAll(textClass);
+      let targetDiv = document.querySelector(targetClass);
+
+      let endX = targetDiv.getBoundingClientRect().left;
+      let endY =
+        targetDiv.getBoundingClientRect().top + targetDiv.offsetHeight / 2;
+
+      textElements.forEach((textToMove) => {
+        let startX = textToMove.getBoundingClientRect().left;
+        let startY = textToMove.getBoundingClientRect().top;
+        let finalY = endY - textToMove.offsetHeight / 2;
+
+        gsap.to(textToMove, {
+          x: endX - startX,
+          y: finalY - startY,
+          scrollTrigger: {
+            trigger: ".teleport-component",
+            start: "2% top",
+            end: "center top",
+            scrub: 0.5,
+          },
+        });
+      });
+    }
+
+    // Just call these once
+    animateText(".move-text-1", ".target-div-1");
+    animateText(".move-text-2", ".target-div-2");
+
+    document.addEventListener("DOMContentLoaded", function () {
+      // Initialize Swiper
+      var swiper = new Swiper(".fiverr", {
+        effect: "cards",
+        grabCursor: true,
+        keyboard: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        },
+        navigation: {
+          nextEl: $(this).find(".swiper-next")[0],
+          prevEl: $(this).find(".swiper-prev")[0],
+          disabledClass: "is-disabled",
+        },
+      });
+
+      // Initialize Swiper
+      var swiper = new Swiper(".upwork", {
+        effect: "cards",
+        grabCursor: true,
+        keyboard: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        },
+        navigation: {
+          nextEl: $(this).find(".upwork-next")[0],
+          prevEl: $(this).find(".upwork-prev")[0],
+          disabledClass: "is-disabled",
+        },
+      });
+
+      // Append elements
+      $(".span-wrapper").each(function (index) {
+        let relatedEl = $(".span-element").eq(index);
+        relatedEl.appendTo($(this));
+      });
+
+      // SVG color manipulation
+      document.querySelectorAll(".logo__single svg path").forEach((path) => {
+        const originalColor = path.getAttribute("fill");
+        path.style.setProperty("--original-color", originalColor);
+        path.setAttribute("fill", "currentColor");
+      });
+
+    });
+  },);
+
+  useEffect(() => {
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+
+    window.onpageshow = function (event) {
+      if (event.persisted) {
+        window.scrollTo(0, 0);
+        window.location.reload(true);
+      }
+    };
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener("DOMContentLoaded", function () {
+      // create svg elements
+      $(".svg-code").each(function (index) {
+        let svgCode = $(this).text();
+        $(svgCode).insertAfter($(this));
+      });
+
+      const logoChangers = document.querySelectorAll(".logo-changer");
+
+      // Loop through each .logo-changer element and create a ScrollTrigger for it
+      logoChangers.forEach((logoChanger) => {
+        ScrollTrigger.create({
+          trigger: logoChanger,
+          start: "top 3%",
+          end: "bottom 10%",
+          toggleClass: {
+            targets: ".nav__logo-wrapper",
+            className: "changed-logo",
+          },
+        });
+      });
+
+      // Split text into spans
+      let typeSplit = new SplitType("[text-split]", {
+        types: "lines, words, chars",
+        tagName: "span",
+      });
+
+      // Link timelines to scroll position
+      function createScrollTrigger(triggerElement, timeline) {
+        // Reset tl when scroll out of view past bottom of screen
+        ScrollTrigger.create({
+          trigger: triggerElement,
+          start: "top bottom",
+          onLeaveBack: () => {
+            timeline.progress(0);
+            timeline.pause();
+          },
+        });
+
+        // Play tl when scrolled into view (60% from top of screen)
+        ScrollTrigger.create({
+          trigger: triggerElement,
+          start: "top bottom",
+          onEnter: () => timeline.play(),
+        });
+      }
+      $("[scrub-each-word]").each(function (index) {
+        let tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: $(this),
+            start: "top 70%",
+            end: "top 20%",
+            scrub: 0.5,
+          },
+        });
+        tl.from($(this).find(".char"), {
+          opacity: 0.15,
+          duration: 0.3,
+          ease: "power1.out",
+          stagger: { each: 0.4 },
+        });
+      });
+
+      $("[letters-slide-up]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.from($(this).find(".word"), {
+          yPercent: 130,
+          duration: 0.9,
+          ease: "power4.out",
+          stagger: { amount: 0.3 },
+        });
+        createScrollTrigger($(this), tl);
+      });
+
+      // Avoid flash of unstyled content
+      gsap.set("[text-split]", { opacity: 1 });
+
+      // Copy footer mail
+      window.withJquery = function () {
+        console.time("time1");
+        var temp = $("<input>");
+        $("body").append(temp);
+        temp.val($(".is-mail-to-copy").text()).select();
+        document.execCommand("copy");
+        temp.remove();
+        console.timeEnd("time1");
+      };
+    });
+  }, [])
+
+  useEffect(() => {
+    let splitText;
+    function runSplit() {
+      splitText = new SplitType("[stagger-link]", {
+        types: "words, chars",
+      });
+    }
+    runSplit();
+
+    // ————— Update on window resize
+    let windowWidth = $(window).innerWidth();
+    window.addEventListener("resize", function () {
+      if (windowWidth !== $(window).innerWidth()) {
+        windowWidth = $(window).innerWidth();
+        splitText.revert();
+        runSplit();
+      }
+    });
+
+    // ————— animation
+    const staggerLinks = document.querySelectorAll("[stagger-link]");
+    staggerLinks.forEach((link) => {
+      const letters = link.querySelectorAll("[stagger-link-text] .char");
+      link.addEventListener("mouseenter", function () {
+        gsap.to(letters, {
+          yPercent: -130,
+          duration: 0.4,
+          ease: "power4.inOut",
+          stagger: { each: 0.025, from: "start" },
+          overwrite: true,
+        });
+      });
+      link.addEventListener("mouseleave", function () {
+        gsap.to(letters, {
+          yPercent: 0,
+          duration: 0.3,
+          ease: "power2.out",
+          stagger: { each: 0.025 },
+          overwrite: true,
+        });
+      });
+    });
+  }, [])
+
+  useEffect(() => {
+
+    window.addEventListener("scroll", function () {
+      var buttons = document.getElementsByClassName("back-to-top");
+      for (var i = 0; i < buttons.length; i++) {
+        if (window.scrollY > 20) {
+          buttons[i].style.display = "flex";
+        } else {
+          buttons[i].style.display = "none";
+        }
+      }
+    });
+
+    // Scroll to the top with a 2-second duration when any button is clicked
+    var buttons = document.getElementsByClassName("back-to-top");
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function () {
+        scrollToTop(2000); // 2 seconds duration
+      });
+    }
+
+    // Function to scroll to the top with a timing-based animation
+    function scrollToTop(duration) {
+      var start = window.pageYOffset;
+      var startTime = performance.now();
+
+      function scrollStep(timestamp) {
+        var currentTime = timestamp || performance.now();
+        var elapsedTime = currentTime - startTime;
+
+        window.scrollTo(
+          0,
+          easeInOutCubic(elapsedTime, start, -start, duration)
+        );
+
+        if (elapsedTime < duration) {
+          requestAnimationFrame(scrollStep);
+        }
+      }
+
+      function easeInOutCubic(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t * t + b;
+        t -= 2;
+        return (c / 2) * (t * t * t + 2) + b;
+      }
+
+      requestAnimationFrame(scrollStep);
+    }
+
+
+
+    document.querySelectorAll(".w--current").forEach((el) => {
+      let innerDiv = el.querySelector("[stagger-link-text]");
+      if (innerDiv) {
+        innerDiv.setAttribute("stagger-link-text", "orange");
+      }
+    });
+  }, [])
+
+
+  useEffect(() => {
+  })
+
+  useEffect(() => {
+    window.Webflow && window.Webflow.destroy();
+    window.Webflow && window.Webflow.ready();
+    window.Webflow && window.Webflow.require('ix2').init();
+    document.dispatchEvent(new Event('readystatechange'))
+    document.dispatchEvent(new Event('DOMContentLoaded'))
+  })
+
+
+
   return (
-    <body className="body">
+    <div className="body">
       <div className="globals">
         <div className="w-embed"></div>
         {/* <!-- ------------------------------------------------------------------ NAVIGATION------------------------------------------------------------------ --> */}
@@ -526,7 +878,7 @@ const Home = () => {
                     <div className="home-cta_span2">
                       <div className="span-element">
                         <img
-                          src="images/Mask group.png"
+                          src={MaskGroup}
                           loading="eager"
                           data-w-id="53e6f3bd-abed-3d64-d7c3-502192f03ad0"
                           sizes="100vw"
@@ -3919,7 +4271,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </body>
+    </div>
   );
 };
 export default Home;
